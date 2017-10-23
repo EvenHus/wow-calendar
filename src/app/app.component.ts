f26c30b83f034b1b9a3b31a3ca2f02f3417f12a3
Even Hus <even.hus.94@gmail.com>
2017-10-23 18:38:33 +0200

    added routing and login component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'WoW Calendar';
+  isLoggedIn = false;
+
+  constructor(private _router: Router) {
+  }
+
+  ngOnInit() {
+  }
+
+  checkLogin(): void {
+    if (this.isLoggedIn) {
+      console.log('You are logged in');
+    } else {
+      console.log('hello');
+      this._router.navigate(['login']);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import {appRoutes} from './app.router';
+import {LoginModule} from './login/login.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
