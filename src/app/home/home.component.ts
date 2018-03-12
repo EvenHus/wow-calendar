9bc81f32bd830e5d32143cf1bbd2094790dd7de2
Even Hus <even.hus.94@gmail.com>
2018-03-12 21:16:48 +0100

    style updates

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,9 @@
 import {Component, OnChanges, OnDestroy, OnInit} from '@angular/core';
 import {ApiService} from '../core/api.service';
 import {ActivatedRoute} from '@angular/router';
+import {Observable} from 'rxjs/Observable';
+import {Store} from '@ngrx/store';
+import * as rootState from '../store';
 
 @Component({
   moduleId: module.id,
@@ -11,14 +14,15 @@ export class HomeComponent implements OnInit {
   name: string;
   realm: string;
   tab: string;
-
+  loading$: Observable<any>;
 
   realmList: string[] = ['Dragonblight', 'Aggramar', 'Outland', 'Stormscale'];
 
-  constructor(private _activatedRoute: ActivatedRoute) {
+  constructor(private _activatedRoute: ActivatedRoute, private _store: Store<rootState.IAppState>) {
   }
 
   ngOnInit() {
+    this.loading$ = this._store.select(rootState.getDataLoadingState);
     const params = this._activatedRoute.snapshot.params;
     this.name = params.user;
     this.realm = params.realm;
