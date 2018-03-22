845c2ff8d054feee2a224b28184497a78426cece
Even Hus <even.hus.94@gmail.com>
2018-03-22 14:59:00 +0100

    Handeling auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,62 @@
-import {Component, OnChanges, OnInit} from '@angular/core';
+import {Component, OnChanges, OnDestroy, OnInit} from '@angular/core';
 import {NavigationStart, Router} from '@angular/router';
 import {Subscription} from 'rxjs/Subscription';
 import {Store} from '@ngrx/store';
 import * as rootState from './store/index';
 import {AuthService} from './core/auth.service';
-import {LocalStorageService} from 'ngx-webstorage';
+import * as AuthActions from './store/auth/auth.actions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent implements OnInit {
-  isLoggedIn: boolean;
+export class AppComponent implements OnInit, OnDestroy {
   authSubscription: Subscription;
   routerEventSubscription: Subscription;
+  currentUrl: string;
 
   constructor(private _router: Router, private _store: Store<rootState.IAppState>,
-              private _authService: AuthService, private _ls: LocalStorageService) {
+              private _authService: AuthService) {
   }
 
   ngOnInit(): void {
-    this.authSubscription = this._store.select(rootState.getAuthentication).subscribe(authenticated => {
-      console.log(authenticated);
-      if (!authenticated) {
-        this._router.navigate(['login']);
-      } else {
-        this._authService.checkToken();
-        this._router.navigate(['home']);
+   this._setCurrentUrl(this._router.events);
+   this._checkAuthentication();
+   this._handleAuthentication();
+   this._store.dispatch(new AuthActions.LoadLoggedInUser());
+  }
+
+  ngOnDestroy(): void {
+    if (this.routerEventSubscription) {
+      this.routerEventSubscription.unsubscribe();
+    }
+  }
+
+  private _setCurrentUrl(event): void {
+    if (event instanceof NavigationStart) {
+      this.currentUrl = event.url;
+      console.log(this.currentUrl);
+    }
+  }
+
+  private _checkAuthentication(): void {
+    this.authSubscription = this._store.select(rootState.getAuthenticated).subscribe(isAuthenticated => {
+      if (isAuthenticated !== null) {
+        if (isAuthenticated) {
+          this._router.navigate(['home']);
+        } else {
+          this._router.navigate(['login']);
+        }
       }
     });
   }
 
   private _handleAuthentication(): void {
     this.routerEventSubscription = this._router.events.subscribe(events => {
-      if (events instanceof NavigationStart && events.url !== '/login') {
-        this._authService.checkToken();
+      if (events instanceof NavigationStart) {
+        if (events.url !== '/login') {
+          this._authService.checkToken();
+        }
       }
     });
   }
